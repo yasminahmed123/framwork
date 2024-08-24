7bb13ea62c6a49897b3eefffd3f2da7f2351a5bb
Yasmin <yasminahmed123>
2024-08-24 03:05:25 +0300

    e-commerce

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertErrorComponent } from "../../shared/ui/alert-error/alert-error.component";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, AlertErrorComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+     register= new FormGroup({
+          name: new FormControl(null,[Validators.required , Validators.maxLength(20),Validators.minLength(2)]),
+          email:new FormControl(null,[Validators.required, Validators.email]),
+          password:new FormControl(null, [Validators.required ,Validators.pattern(/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!#$%&?]).{8,}$/)]),
+          rePassword:new FormControl(null, [Validators.required ,Validators.pattern(/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!#$%&?]).{8,}$/)]),
+         
 
+     })
+
+     sendData(){
+      console.log(this.register)
+     }
 }
---
+++ src/app/shared/ui/alert-error/alert-error.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'alertError',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert-error.component.html',
+  styleUrls: ['./alert-error.component.scss']
+})
+export class AlertErrorComponent {
+  @Input() formName!: FormGroup;
+  @Input() controlName!:string;
+}
